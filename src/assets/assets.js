47fc3bf7af640d13fa93688f224fdf97a1d92924
M.N.M. Nusair <nusair2412@gmail.com>
2026-02-27 15:17:31 +0530

    Initial commit - Invoice App setup with Vite

---
+++ src/assets/assets.js
@@ -0,0 +1,32 @@
+import pic1 from    './images/pic1.jpg';
+import pic2 from    './images/pic2.jpg';
+import pic3 from    './images/pic3.jpg';
+import pic4 from    './images/pic4.jpg';    
+import pic5 from    './images/pic5.jpg';
+import pic6 from    './images/pic6.jpg';
+import pic7 from    './images/pic7.jpg';
+import pic8 from    './images/pic8.jpg';
+import pic9 from    './images/pic9.jpg';
+import pic10 from   './images/pic10.jpg';
+import pic11 from   './images/pic11.jpg';
+import pic12 from   './images/pic12.jpg';
+import pic13 from   './images/pic13.jpg';
+import logo from   './images/logo.png';
+
+
+export const assets = {
+     pic1,
+     pic2,
+     pic3,
+     pic4,
+     pic5,
+     pic6,
+     pic7,
+     pic8,
+     pic9,
+     pic10,
+     pic11,
+     pic12,
+     pic13,
+     logo
+} 
